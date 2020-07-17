114390349ab709934cab4b2fcd1303c064fd65f2
Ivan <strokovhii@gmail.comgit config --global user.name Ivangit config --global user.email strokovhii@gmail.com>
2020-07-17 14:38:03 +0700

    ie js fixes

--- source/js/bundle.js
+++ source/js/bundle.js
@@ -2956,50 +2956,7 @@ module.exports = __webpack_require__(/*! ./dist/inputmask */ "./node_modules/inp
 
   console.log(userData);
 
-  var returnPopupTemplate = function () {
-    return (
-      `<div class="popup-call">
-        <div class="popup-call__container">
-          <button class="popup-call__close-button" type="button">
-            <span class="visually-hidden">закрыть окно</span>
-          </button>
-          <h2 class="popup-call__heading">Закажите звонок</h2>
-          <p class="popup-call__description">Оставьте контакты, мы проконсультируем вас бесплатно в удобное время</p>
-          <form class="popup-call__feedback-form" action="#" method="post">
-            <fieldset class="popup-call__fieldset">
-              <ul class="popup-call__list">
-                <li class="popup-call__item">
-                  <label class="visually-hidden" for="popup-call-user-name">Ваше имя:</label>
-                  <input autofocus class="popup-call__input input" type="text" name="popup-call-user-name" id="popup-call-user-name" placeholder="Имя"
-                  pattern="^[А-Яа-яЁё\s]+$" required>
-                  <span class="popup-call__input-error input-error">Введите имя русскими буквами</span>
-                </li>
-                <li class="popup-call__item">
-                  <label class="visually-hidden" for="popup-call-user-tel">Ваш телефон:</label>
-                  <input class="popup-call__input input" type="tel" name="popup-call-user-tel" id="popup-call-user-tel" placeholder="Телефон"
-                  required>
-                  <span class="popup-call__input-error input-error">Введите телефон: +7 (123) 456 78 90</span>
-                </li>
-                <li class="popup-call__item popup-call__item--textarea">
-                  <label class="visually-hidden" for="popup-call-user-question">Ваш вопрос:</label>
-                  <textarea name="text" placeholder="Ваш вопрос" cols="30" rows="6" id="popup-call-user-question"></textarea>
-                </li>
-              </ul>
-            </fieldset>
-            <button class="popup-call__button button" type="submit">Задать вопрос</button>
-            <input class="popup-call__checkbox visually-hidden" type="checkbox" name="popup-consent" id="popup-consent" value="true" required>
-            <label class="popup-call__label popup-call__label--consent" for="popup-consent">
-              Я согласен на обработку  персональных данных
-            </label>
-          </form>
-        </div>
-      </div>`
-    );
-  };
-
   var renderPopup = function () {
-    render(mainSection, returnPopupTemplate(), 'beforeend');
-
     var questionPopup = document.querySelector('.popup-call');
     var closeButton = questionPopup.querySelector('.popup-call__close-button');
     var submitButton = questionPopup.querySelector('.popup-call__button');
@@ -3034,16 +2991,15 @@ module.exports = __webpack_require__(/*! ./dist/inputmask */ "./node_modules/inp
       }
 
       console.log(userData);
-
       setUserData(userData);
 
-      popup.remove();
+      popup.classList.remove('popup-call--active');
       document.body.style.overflow = 'auto';
       document.removeEventListener('keydown', onPopupEscPress);
       document.removeEventListener('click', onPopupClose);
     };
 
-    const onPopupEscPress = function (evt) {
+    var onPopupEscPress = function (evt) {
       if (evt.keyCode === ESC_KEY) {
         onPopupClose();
       }
@@ -3053,15 +3009,16 @@ module.exports = __webpack_require__(/*! ./dist/inputmask */ "./node_modules/inp
     document.addEventListener('keydown', onPopupEscPress);
   };
 
-  var onCallButtonClick = (evt) => {
+  var onCallButtonClick = function (evt) {
     evt.preventDefault();
     var popup = document.querySelector('.popup-call');
-    if (!popup) {
+    if (!popup.classList.contains('popup-call--active')) {
+      popup.classList.add('popup-call--active');
       renderPopup();
     }
   };
 
-  var onSubmitButtonClick = (evt) => {
+  var onSubmitButtonClick = function (evt) {
     evt.preventDefault();
   };
 
